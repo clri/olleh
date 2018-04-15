2c9225e1df2cec7b51c5bdec54f6e39c856af6e9
mmb2276 <mmb2276@columbia.edu>
2018-04-15 18:13:55 -0400

    Added list.c

---
+++ list.c
@@ -0,0 +1,15 @@
+/* creates linked list of chars or linked lists.
+used zentut.com's linked list tutorial to implement. */
+
+#define NULL 0
+
+struct charnode{
+    char data;
+    struct charnode *next;
+};
+
+//typedef struct listnode{
+
+int main() {
+    struct charnode c = {'a', NULL};
+}
